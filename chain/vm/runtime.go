e0dd42fe5a8c7a6c0beb136c8b8759f1311abfda
Aayush Rajasekaran <arajasek94@gmail.com>
2020-03-21 03:27:27 -0400

    Transfer balance of deleted actor to beneficiary

--- chain/vm/runtime.go
+++ chain/vm/runtime.go
@@ -194,14 +194,16 @@ func (rt *Runtime) CreateActor(codeId cid.Cid, address address.Address) {
 	}
 }
 
-func (rt *Runtime) DeleteActor() {
+func (rt *Runtime) DeleteActor(beneficiary address.Address) {
 	rt.ChargeGas(rt.Pricelist().OnDeleteActor())
 	act, err := rt.state.GetActor(rt.Message().Receiver())
 	if err != nil {
 		rt.Abortf(exitcode.SysErrInternal, "failed to load actor in delete actor: %s", err)
 	}
 	if !act.Balance.IsZero() {
-		rt.Abortf(exitcode.SysErrInternal, "cannot delete actor with non-zero balance")
+		if err := rt.vm.transfer(rt.Message().Receiver(), beneficiary, act.Balance); err != nil {
+			rt.Abortf(exitcode.SysErrInternal, "failed to transfer deleted actor's balance to beneficiary: %s", err)
+		}
 	}
 	if err := rt.state.DeleteActor(rt.Message().Receiver()); err != nil {
 		rt.Abortf(exitcode.SysErrInternal, "failed to delete actor: %s", err)
